70f3e80e12ce7f3464016dd63e2e542d262bd32c
GET Academy <terje@kolderup.net>
2024-03-14 13:15:25 +0100

    x

--- js/previewView.js
+++ js/previewView.js
@@ -3,18 +3,25 @@ function updateViewPreview(){
     const movie = model.movies[index];
 
     document.getElementById('app').innerHTML = /*HTML*/`
-        <h1>Pause til 13:08</h1>
-
-        <h1>${movie.title}</h1>
         <img 
             src="${movie.thumbnail}"
             style="
+                float: right;
                 width: ${movie.thumbnail_width*4}; 
                 height: ${movie.thumbnail_height*4}; 
                 "
             />
+        <h1>${movie.title} - ${movie.year}</h1>
+
+        <h3>Skuespillere</h3>
+        <ul>
+            <li>${movie.cast.join('<li>')}
+        </ul>
+        <p>
+            ${movie.extract}
+        </p>
         <div>
-            <button>Tilbake</button>
+            <button onclick="goTo('search')">Tilbake</button>
         </div>
     `;
 }
